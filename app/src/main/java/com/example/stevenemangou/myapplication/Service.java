03565c86167125dd8c95657141e8e907b5fed39d
Steve Nemangou <steve.nemangou-tchatchou@stud.uni-due.de>
2018-12-30 04:06:49 +0100

    new Fragment

---
+++ app/src/main/java/com/example/stevenemangou/myapplication/Service.java
@@ -0,0 +1,14 @@
+package com.example.stevenemangou.myapplication;
+import java.util.Date;
+
+public class Service {
+    Date departuredate ;
+    Date arrivaldate;
+    String DepartureTown;
+    String ArrivalTown;
+
+    public String getDepartureTown(){return DepartureTown;}
+
+    public String getArrivalTown(){return ArrivalTown;}
+    
+}
