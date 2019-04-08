b2a520837c279494359ec33c4accd25dee73fd2b
Steve Nemangou <steve.nemangou-tchatchou@stud.uni-due.de>
2019-04-08 20:25:28 +0200

    new register

---
+++ app/src/main/java/com/example/stevenemangou/myapplication/infoserviceActivity.java
@@ -0,0 +1,13 @@
+package com.example.stevenemangou.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class infoserviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infoservice);
+    }
+}
